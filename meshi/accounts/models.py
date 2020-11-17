bc2b793ca887539446694d17f6f6810b4ba76b4d
Laronk <karpiuk.janek@gmail.com>
2020-11-17 02:07:59 +0100

    Correct django app structure

--- meshi/accounts/models.py
+++ meshi/accounts/models.py
@@ -18,7 +18,6 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-
     def create_superuser(self, email, username, password):
         user = self.create_user(
             email=self.normalize_email(email),
@@ -31,30 +30,28 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-class Account(AbstractBaseUser):
-    id                      = models.AutoField(primary_key=True)
-    email                   = models.EmailField(verbose_name="mail", max_length=64, unique=True)
-    username                = models.CharField(max_length=30, unique=True)
-    date_joined             = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-    last_login              = models.DateTimeField(verbose_name='last login', auto_now=True)
-    is_admin                = models.BooleanField(default=False)
-    is_active               = models.BooleanField(default=True)
-    is_staff                = models.BooleanField(default=False)
-    is_superuser            = models.BooleanField(default=False)
-   # first_name              = models.CharField(max_length=30)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+class Account(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(verbose_name="mail", max_length=64, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    date_joined = models.DateTimeField(verbose_name="date joined", auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
 
     objects = MyAccountManager()
 
     def _str_(self):
-        return self.email 
+        return self.email
 
     def has_perm(self, perm, obj=None):
         return self.is_admin
 
     def has_module_perms(self, app_label):
         return True
-
-    
