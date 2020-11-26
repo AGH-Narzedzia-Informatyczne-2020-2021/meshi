1c1b413b6139a7361b5777bd53f2b7b1ab712021
Laronk <karpiuk.janek@gmail.com>
2020-11-26 23:11:26 +0100

    Restructure django recipe app tests

--- meshi/recipes/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-from recipes.models import recipe
-import test_recipe
-import test_ingredient
-
--- meshi/recipes/tests/models/test_ingredients.py
+++ meshi/recipes/tests/models/test_ingredients.py
@@ -1,33 +1,32 @@
 from django.test import TestCase
 from recipes.models import Ingredient
 
+
 class test_ingredient_name(TestCase):
+    def setUp(self):
+        Ingredient.objects.create(id=1, name="1 teaspoon baking soda")
+
     def test_name_label(self):
         """This function checks if the object has name field"""
-        ingredient = Ingredient.objects.get(id=1)
-        field_label = Ingredient._meta.get_field('name').verbose_name
-        self.assertEqual(field_label, 'name')
+        field_label = Ingredient._meta.get_field("name").verbose_name
+        self.assertEqual(field_label, "name")
 
     def test_name_content(self):
         """This function checks if the object's name isn't Null"""
         ingredient = Ingredient.objects.get(id=1)
-        field_object = Ingredient._meta.get_field('name')
+        field_object = Ingredient._meta.get_field("name")
         content = field_object.value_from_object(ingredient)
-        self.assertNotEqual(content, Null)
+        self.assertNotEqual(content, None)
 
     def test_name_max_length(self):
         """This function checks object's name max length"""
-        ingredient = Ingredient.objects.get(id=1)
-        max_length = Ingredient._meta.get_field('name').max_length
+        max_length = Ingredient._meta.get_field("name").max_length
         self.assertEqual(max_length, 128)
-    
+
     def test_title_length(self):
         """This function checks object's name max length"""
         ingredient = Ingredient.objects.get(id=1)
-        field_object = Ingredient._meta.get_field('name')
+        field_object = Ingredient._meta.get_field("name")
         content = field_object.value_from_object(ingredient)
-        title_length=len(content)
-        score = 0
-        if title_length < 128 :
-            score=1
-        self.assertEqual(score, 1)
+        title_length = len(content)
+        self.assertLessEqual(title_length, 128)
