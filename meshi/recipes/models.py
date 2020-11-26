a5e5073830da454774c83192a507a5b64762c3c6
Laronk <karpiuk.janek@gmail.com>
2020-11-26 23:07:57 +0100

    Fix dependency issue between models

--- meshi/recipes/models.py
+++ meshi/recipes/models.py
@@ -2,23 +2,6 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-class Recipe(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=256)
-    instructions = models.TextField()
-    ingredients = models.ManyToManyField(ingredient)
-    image = models.URLField(max_length=256)
-    ease_of_prep = models.IntegerField(
-        validators=[MaxValueValidator(10), MinValueValidator(1)]
-    )
-    rating = models.IntegerField(
-        validators=[MaxValueValidator(10), MinValueValidator(1)]
-    )
-    prep_time = models.IntegerField(validators=[MinValueValidator(1)])
-    notes = models.CharField(max_length=256)
-    types = models.CharField(max_length=256)
-
-
 class Ingredient(models.Model):
     """
     Examples from our datebase:
@@ -31,4 +14,22 @@ class Ingredient(models.Model):
     so each element is a string
     """
 
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=128)
+
+
+class Recipe(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256)
+    instructions = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient)
+    image = models.URLField(max_length=256)
+    ease_of_prep = models.IntegerField(
+        validators=[MaxValueValidator(10), MinValueValidator(1)]
+    )
+    rating = models.IntegerField(
+        validators=[MaxValueValidator(10), MinValueValidator(1)]
+    )
+    prep_time = models.IntegerField(validators=[MinValueValidator(1)])
+    notes = models.CharField(max_length=256)
+    types = models.CharField(max_length=256)
