a9a0a43b0c78fba7a820bb9a57f6600404b9dabd
Daniel Henel <danielhenel@student.agh.edu.pl>
2020-11-30 21:43:09 +0100

    admin registration update

--- meshi/recipes/admin.py
+++ meshi/recipes/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Recipe
 from .models import Ingredient
 
-admin.site.register(Recipe)
 admin.site.register(Ingredient)
+
+class RecipeAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "id",
+        "title",
+        "ease_of_prep",
+        "rating",
+        "prep_time", 
+    )
+
+    search_fields = (
+        "title",
+        "rating",
+    )
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Recipe, RecipeAdmin)
