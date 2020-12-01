e8bf4b3d794bc4ce562a7929fb02993f818a7b36
Laronk <karpiuk.janek@gmail.com>
2020-12-01 01:38:01 +0100

    Add filters to Recipie and Ingredient admin view

--- meshi/recipes/admin.py
+++ meshi/recipes/admin.py
@@ -1,26 +1,55 @@
 from django.contrib import admin
-from .models import Recipe
-from .models import Ingredient
+from recipes.models import Recipe
+from recipes.models import Ingredient
 
-admin.site.register(Ingredient)
+
+class IngredientAdmin(admin.ModelAdmin):
+
+    list_display = ("id", "name")
+
+    search_fields = ("name",)
+
+    filter_horizontal = ()
+    list_filter = ()
+
 
 class RecipeAdmin(admin.ModelAdmin):
 
     list_display = (
         "id",
         "title",
+        "instructions",
+        "get_ingredients",
+        "image",
         "ease_of_prep",
         "rating",
-        "prep_time", 
+        "prep_time",
+        "notes",
+        "types",
     )
 
     search_fields = (
         "title",
+        "instructions",
+        "ingredients__name",
+        "ease_of_prep",
         "rating",
+        "prep_time",
+        "types",
     )
 
-    filter_horizontal = ()
-    list_filter = ()
-    fieldsets = ()
+    filter_horizontal = ("ingredients",)
+    list_filter = (
+        "ease_of_prep",
+        "rating",
+        "prep_time",
+        "types",
+        "ingredients__name",
+    )
 
+    def get_ingredients(self, recipe):
+        return "\n".join([ingredient.name for ingredient in recipe.ingredients.all()])
+
+
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
